4e7d9e1fba5eaa6c532f3c061086e8469ebdf5ec
mattiatj <johann.mattiat@gmail.com>
2023-11-28 15:32:08 +0100

    add test for data files

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 import numpy as np
 import test_meson.fortran_code as fc
+import importlib.resources as resources
+from pathlib import Path
+
+DATADIR = Path.joinpath(resources.files("test_meson"), "data")
 
 def square(x):
     result = x**2
@@ -13,6 +17,10 @@ def numpy_square(x):
 def fortran_square(x):
     result = fc.fortran_code.fast_square(x)
     return result
+
+def test_data_files():
+    data = np.loadtxt(DATADIR.joinpath("data.dat"))
+    print(data)
 
 if __name__=="__main__":
     print(square(2.0))
